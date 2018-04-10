5698ff0afe3a09f80384ea56713e554331e981b8
asheni <ashenikaperera@gmail.com>
2018-04-10 13:36:16 +0530

    init

---
+++ src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { AddComponent } from './add/add.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    ContactComponent,
+    AddComponent
+  ],
+  imports: [
+    BrowserModule,
+      FormsModule,
+      HttpModule,
+      RouterModule.forRoot([
+          { path: 'home', component: HomeComponent},
+          { path: 'contact', component: ContactComponent},
+          { path: 'add', component: AddComponent}
+      ])
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
